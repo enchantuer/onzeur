f5d4cb8ddf188130778bd6f5305253b26fe0e62f
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-05-31 01:00:52 +0200

    database information

--- php/constants.php
+++ php/constants.php
@@ -1,7 +1,7 @@
 <?php
 // Database constants.
-const DB_USER = '';
-const DB_PASSWORD = '';
-const DB_NAME = '';
+const DB_USER = 'jarvis';
+const DB_PASSWORD = 'password';
+const DB_NAME = 'onzeur';
 const DB_SERVER = 'localhost';
 const DB_PORT = '5432';
