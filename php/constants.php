15b2535ab78e8f624eeefcfd88142fc1c4ce66be
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-05-30 16:48:59 +0200

    Connection to database

---
+++ php/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Database constants.
+const DB_USER = '';
+const DB_PASSWORD = '';
+const DB_NAME = '';
+const DB_SERVER = 'localhost';
+const DB_PORT = '5432';
---
+++ php/database.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'constants.php';
+function db_connect() {
+    try  {
+        $db = new PDO('pgsql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $exception)  {
+        error_log('Connection error: '.$exception->getMessage());
+        return false;
+    }
+    return $db;
+}
