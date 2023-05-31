c1e746ec327e161ff5a59c5b4892740e409528ed
zelmano <zelman.brun@gmail.com>
2023-05-31 13:53:55 +0200

    login file

---
+++ src/auth/login.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title>Sign Up</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<div class="form">
+<h1>Login</h1>
+<p>Don't have an account? <a href='signup.php'>SIGN UP</a></p>
+
+<form name="login" action="" method="post">
+  <label for="email">Email:</label>
+  <input type="email" name="email" placeholder="Email" required/>
+  <label for="password">Password:</label>
+  <input type="password" name="password" placeholder="Password" required/>
+  <input class="button" type="submit" name="submit" value="Login"/>
+</form>
+
+</div>
+
+</body>
+</html>
