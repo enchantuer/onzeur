89a897b1b38d96e6e682811c97e62a498bca16f3
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-05-31 09:47:27 +0200

    add return type
    false|array

--- php/get.php
+++ php/get.php
@@ -1,54 +1,54 @@
 <?php
 
-function dbGetArtistes(PDO $db) {
+function dbGetArtistes(PDO $db): false|array {
     $query = $db->prepare('SELECT * FROM artist_');
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function dbGetArtist(PDO $db, int $id) {
+function dbGetArtist(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM artist_ WHERE id_artist = :id');
     $query->execute([':id' => $id]);
     return $query->fetch(PDO::FETCH_ASSOC);
 }
 
-function dbGetAlbums(PDO $db) {
+function dbGetAlbums(PDO $db): false|array {
     $query = $db->prepare('SELECT * FROM album_');
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function dbGetAlbum(PDO $db, int $id) {
+function dbGetAlbum(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM album_ WHERE id_album = :id');
     $query->execute([':id' => $id]);
     return $query->fetch(PDO::FETCH_ASSOC);
 }
 
- function dbGetTracks(PDO $db) {
+ function dbGetTracks(PDO $db): false|array {
     $query = $db->prepare('SELECT * FROM track_');
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function dbGetTrack(PDO $db, int $id) {
+function dbGetTrack(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM track_ WHERE id_track = :id');
     $query->execute([':id' => $id]);
     return $query->fetch(PDO::FETCH_ASSOC);
 }
 
-function dbGetTracksByAlbum(PDO $db, int $id) {
+function dbGetTracksByAlbum(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM track_ WHERE id_album = :id');
     $query->execute([':id' => $id]);
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function dbGetTracksByArtist(PDO $db, int $id) {
+function dbGetTracksByArtist(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM track_ WHERE id_artist = :id');
     $query->execute([':id' => $id]);
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function dbGetAlbumsByArtist(PDO $db, int $id) {
+function dbGetAlbumsByArtist(PDO $db, int $id): false|array {
     $query = $db->prepare('SELECT * FROM album_ WHERE id_artist = :id');
     $query->execute([':id' => $id]);
     return $query->fetchAll(PDO::FETCH_ASSOC);
