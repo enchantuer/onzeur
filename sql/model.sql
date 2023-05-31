c941dc9fe3fb1deba242a83664f018730cd3f8f6
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-05-31 10:03:34 +0200

    update name + add favorite playlist

--- sql/model.sql
+++ sql/model.sql
@@ -13,41 +13,13 @@ DROP TABLE IF EXISTS avoir CASCADE ;
 DROP TABLE IF EXISTS contient CASCADE ;
 
 ------------------------------------------------------------
--- Table: user_
-------------------------------------------------------------
-CREATE TABLE public.user_(
-                             id_user      SERIAL NOT NULL ,
-                             first_name   VARCHAR (50) NOT NULL ,
-                             name         VARCHAR (50) NOT NULL ,
-                             email        VARCHAR (50) NOT NULL ,
-                             birth_date   DATE  NOT NULL ,
-                             password     VARCHAR (50) NOT NULL ,
-                             CONSTRAINT user__PK PRIMARY KEY (id_user)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: playlist_
-------------------------------------------------------------
-CREATE TABLE public.playlist_(
-                                 id_playlist     SERIAL NOT NULL ,
-                                 name            VARCHAR (50) NOT NULL ,
-                                 creation_date   DATE  NOT NULL ,
-                                 id_user         INT  NOT NULL  ,
-                                 CONSTRAINT playlist__PK PRIMARY KEY (id_playlist)
-
-    ,CONSTRAINT playlist__user__FK FOREIGN KEY (id_user) REFERENCES public.user_(id_user)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
 -- Table: music_type_
 ------------------------------------------------------------
 CREATE TABLE public.music_type_(
                                    id_type   SERIAL NOT NULL ,
                                    type      VARCHAR (50) NOT NULL  ,
                                    CONSTRAINT music_type__PK PRIMARY KEY (id_type)
-)WITHOUT OIDS;
+);
 
 
 ------------------------------------------------------------
@@ -57,7 +29,7 @@ CREATE TABLE public.artist_type_(
                                     id_type   SERIAL NOT NULL ,
                                     type      VARCHAR (50) NOT NULL  ,
                                     CONSTRAINT artist_type__PK PRIMARY KEY (id_type)
-)WITHOUT OIDS;
+);
 
 
 ------------------------------------------------------------
@@ -68,9 +40,7 @@ CREATE TABLE public.artist_(
                                name        VARCHAR (50) NOT NULL ,
                                id_type     INT  NOT NULL  ,
                                CONSTRAINT artist__PK PRIMARY KEY (id_artist)
-
-    ,CONSTRAINT artist__artist_type__FK FOREIGN KEY (id_type) REFERENCES public.artist_type_(id_type)
-)WITHOUT OIDS;
+);
 
 
 ------------------------------------------------------------
@@ -84,10 +54,7 @@ CREATE TABLE public.album_(
                               id_artist      INT  NOT NULL ,
                               id_type        INT  NOT NULL  ,
                               CONSTRAINT album__PK PRIMARY KEY (id_album)
-
-    ,CONSTRAINT album__artist__FK FOREIGN KEY (id_artist) REFERENCES public.artist_(id_artist)
-    ,CONSTRAINT album__music_type_0_FK FOREIGN KEY (id_type) REFERENCES public.music_type_(id_type)
-)WITHOUT OIDS;
+);
 
 
 ------------------------------------------------------------
@@ -100,38 +67,118 @@ CREATE TABLE public.track_(
                               id_album    INT  NOT NULL ,
                               id_artist   INT  NOT NULL  ,
                               CONSTRAINT track__PK PRIMARY KEY (id_track)
-
-    ,CONSTRAINT track__album__FK FOREIGN KEY (id_album) REFERENCES public.album_(id_album)
-    ,CONSTRAINT track__artist_0_FK FOREIGN KEY (id_artist) REFERENCES public.artist_(id_artist)
-)WITHOUT OIDS;
+);
 
 
 ------------------------------------------------------------
--- Table: Avoir
+-- Table: user_
 ------------------------------------------------------------
-CREATE TABLE public.Avoir(
-                             id_track      INT  NOT NULL ,
-                             id_playlist   INT  NOT NULL ,
-                             add_date      DATE  NOT NULL  ,
-                             CONSTRAINT Avoir_PK PRIMARY KEY (id_track,id_playlist)
-
-    ,CONSTRAINT Avoir_track__FK FOREIGN KEY (id_track) REFERENCES public.track_(id_track)
-    ,CONSTRAINT Avoir_playlist_0_FK FOREIGN KEY (id_playlist) REFERENCES public.playlist_(id_playlist)
-)WITHOUT OIDS;
+CREATE TABLE public.user_(
+                             id_user       SERIAL NOT NULL ,
+                             first_name    VARCHAR (50) NOT NULL ,
+                             name          VARCHAR (50) NOT NULL ,
+                             email         VARCHAR (50) NOT NULL ,
+                             birth_date    DATE  NOT NULL ,
+                             password      VARCHAR (50) NOT NULL ,
+                             id_playlist_favorite   INT  NOT NULL  ,
+                             CONSTRAINT user__PK PRIMARY KEY (id_user)
+);
 
 
 ------------------------------------------------------------
--- Table: Contient
+-- Table: playlist_
 ------------------------------------------------------------
-CREATE TABLE public.Contient(
+CREATE TABLE public.playlist_(
+                                 id_playlist     SERIAL NOT NULL ,
+                                 name            VARCHAR (50) NOT NULL ,
+                                 creation_date   DATE  NOT NULL ,
+                                 id_user         INT  NOT NULL  ,
+                                 CONSTRAINT playlist__PK PRIMARY KEY (id_playlist)
+);
+
+
+------------------------------------------------------------
+-- Table: playlist_track_
+------------------------------------------------------------
+CREATE TABLE public.playlist_track_(
+                                       id_track      INT  NOT NULL ,
+                                       id_playlist   INT  NOT NULL ,
+                                       add_date      DATE  NOT NULL  ,
+                                       CONSTRAINT playlist_track__PK PRIMARY KEY (id_track,id_playlist)
+);
+
+
+------------------------------------------------------------
+-- Table: history_
+------------------------------------------------------------
+CREATE TABLE public.history_(
                                 id_track   INT  NOT NULL ,
                                 id_user    INT  NOT NULL ,
                                 add_date   DATE  NOT NULL  ,
-                                CONSTRAINT Contient_PK PRIMARY KEY (id_track,id_user)
+                                CONSTRAINT history__PK PRIMARY KEY (id_track,id_user)
+);
 
-    ,CONSTRAINT Contient_track__FK FOREIGN KEY (id_track) REFERENCES public.track_(id_track)
-    ,CONSTRAINT Contient_user_0_FK FOREIGN KEY (id_user) REFERENCES public.user_(id_user)
-)WITHOUT OIDS;
+
+
+
+ALTER TABLE public.artist_
+    ADD CONSTRAINT artist__artist_type_0_FK
+        FOREIGN KEY (id_type)
+            REFERENCES public.artist_type_(id_type);
+
+ALTER TABLE public.album_
+    ADD CONSTRAINT album__artist_0_FK
+        FOREIGN KEY (id_artist)
+            REFERENCES public.artist_(id_artist);
+
+ALTER TABLE public.album_
+    ADD CONSTRAINT album__music_type_1_FK
+        FOREIGN KEY (id_type)
+            REFERENCES public.music_type_(id_type);
+
+ALTER TABLE public.track_
+    ADD CONSTRAINT track__album_0_FK
+        FOREIGN KEY (id_album)
+            REFERENCES public.album_(id_album);
+
+ALTER TABLE public.track_
+    ADD CONSTRAINT track__artist_1_FK
+        FOREIGN KEY (id_artist)
+            REFERENCES public.artist_(id_artist);
+
+ALTER TABLE public.user_
+    ADD CONSTRAINT user__playlist_0_FK
+        FOREIGN KEY (id_playlist_favorite)
+            REFERENCES public.playlist_(id_playlist);
+
+ALTER TABLE public.user_
+    ADD CONSTRAINT user__playlist_0_AK
+        UNIQUE (id_playlist_favorite);
+
+ALTER TABLE public.playlist_
+    ADD CONSTRAINT playlist__user_0_FK
+        FOREIGN KEY (id_user)
+            REFERENCES public.user_(id_user);
+
+ALTER TABLE public.playlist_track_
+    ADD CONSTRAINT playlist_track__track_0_FK
+        FOREIGN KEY (id_track)
+            REFERENCES public.track_(id_track);
+
+ALTER TABLE public.playlist_track_
+    ADD CONSTRAINT playlist_track__playlist_1_FK
+        FOREIGN KEY (id_playlist)
+            REFERENCES public.playlist_(id_playlist);
+
+ALTER TABLE public.history_
+    ADD CONSTRAINT history__track_0_FK
+        FOREIGN KEY (id_track)
+            REFERENCES public.track_(id_track);
+
+ALTER TABLE public.history_
+    ADD CONSTRAINT history__user_1_FK
+        FOREIGN KEY (id_user)
+            REFERENCES public.user_(id_user);
 
 
 
