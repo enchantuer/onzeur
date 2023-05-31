d7e715190ce13122e9e649d1ba2bb49d1654ca4d
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-05-31 16:13:33 +0200

    Base class to interact with db

---
+++ API/DatabaseInteraction.php
@@ -0,0 +1,12 @@
+<?php
+
+abstract class DatabaseInteraction {
+    protected static PDO $db;
+
+    public static function connect(): false|PDO {
+        if (!isset(self::$db)) {
+            self::$db = dbConnect();
+        }
+        return self::$db;
+    }
+}
