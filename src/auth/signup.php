f5de9e33ee2b79f3fa41b3adf8b83976788075ce
zelmano <zelman.brun@gmail.com>
2023-06-01 15:06:15 +0200

    even better html

--- src/auth/signup.php
+++ src/auth/signup.php
@@ -35,22 +35,22 @@
 </label>
 
 <script>
-const toggle=document.getElementById('theme');
-const elements=document.getElementsByTagName('*');
+  const toggle=document.getElementById('theme');
+  const elements=document.getElementsByTagName('*');
 
-toggle.addEventListener('input',e=>{
-  const checked=e.target.checked;
+  toggle.addEventListener('input',e=>{
+    const checked=e.target.checked;
 
-  if(checked){
-    for(let i=0;i<elements.length;i++){
-      elements[i].classList.add('dark');
+    if(checked){
+      for(let i=0;i<elements.length;i++){
+        elements[i].classList.add('dark');
+      }
+    }else{
+      for(let i=0;i<elements.length;i++){
+        elements[i].classList.remove('dark');
+      }
     }
-  }else{
-    for(let i=0;i<elements.length;i++){
-      elements[i].classList.remove('dark');
-    }
-  }
-});
+  });
 </script>
 
 </body>
