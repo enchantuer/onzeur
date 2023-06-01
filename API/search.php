ce3202debfae5ae40558b20dbaab0e433a244181
zelmano <zelman.brun@gmail.com>
2023-06-01 08:55:52 +0200

    aaa

--- API/search.php
+++ API/search.php
@@ -1,66 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
+require_once 'DatabaseInteraction.php';
 
-require_once "../php/database.php";
-require_once "Track.php";
-require_once "Album.php";
-require_once "Artist.php";
-require_once "SearchTrack.php";
-require_once "SearchAlbum.php";
-require_once "SearchArtist.php";
+abstract class Search extends DatabaseInteraction {
+    public string $search;
+    public array $results;
 
-function getData() {
-    DatabaseElement::connect();
-    $request = substr($_SERVER['PATH_INFO'], 1);
-    $request = explode('/', $request);
-    $request_resource = array_shift($request);
-    $request_method = $_SERVER['REQUEST_METHOD'];
-
-    if ($request_resource == 'track') {
-        if ($request_method == 'GET') {
-            if (count($request) == 0) {
-                if (isset($_GET['title'])) {
-                    $search = new SearchTrack($_GET['title']);
-                    return $search->find();
-                }
-            } else {
-                $trackId = array_shift($request);
-                return Track::fromId($trackId);
-            }
-        }
+    public function __construct(string $search) {
+        $this->search = $search;
     }
-    elseif ($request_resource == 'album') {
-        if ($request_method == 'GET') {
-            if (count($request) == 0) {
-                if (isset($_GET['title'])) {
-                    $search = new SearchAlbum($_GET['title']);
-                    return $search->find();
-                }
-            } else {
-                $albumId = array_shift($request);
-                return Album::fromId($albumId);
-            }
-        }
-    }
-    elseif ($request_resource == 'artist') {
-        if ($request_method == 'GET') {
-            if (count($request) == 0) {
-                if (isset($_GET['name'])) {
-                    $search = new SearchArtist($_GET['name']);
-                    return $search->find();
-                }
-            } else {
-                $artistId = array_shift($request);
-                return Artist::fromId($artistId);
-            }
-        }
-    }
-//    if ($request_method == 'PUT') {
-//        parse_str(file_get_contents('php://input'), $_PUT);
-//        return db_modify_tweet($conn, $request[0], $_PUT['login'], $_PUT['text']);
-//    }
+
+    abstract function find();
 }
-
-echo json_encode(getData());
