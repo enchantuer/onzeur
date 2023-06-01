b5b0bb81ed66b3bea6155be8568edd78eed71426
Antoine Bertin <antoine.bertin@isen-ouest.yncrea.fr>
2023-06-01 11:39:43 +0200

    sign up

--- src/auth/signup.php
+++ src/auth/signup.php
@@ -29,4 +29,39 @@
 </div>
 
 </body>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include_once '../../php/database.php';
+    require_once '../../php/add.php';
+    $conn = dbConnect();
+
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $birthdate = $_POST['birthdate'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    if ($password != $password2) {
+        echo "Les mots de passe ne correspondent pas.";
+        exit();
+    }
+
+    $success = dbAddUser($conn, $firstname, $lastname, $birthdate, $email, $password);
+    if ($success) {
+        echo "Votre compte a bien été créé.";
+    } else {
+        echo "Une erreur est survenue lors de la création de votre compte.";
+    }
+
+}
+?>
+
+
+
+
 </html>
