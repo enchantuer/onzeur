ca58a38535f75c5d036aa70e41ef2fa2c2938158
zelmano <zelman.brun@gmail.com>
2023-06-04 01:34:28 +0200

    sry

---
+++ src/js/artist_details.js
@@ -0,0 +1,24 @@
+let params = new URLSearchParams(document.location.search);
+let artistId = params.get('id');
+
+if (artistId) {
+  ajaxRequest('GET', '../api.php/artist/' + artistId, function(artist) {
+    console.log(artist);
+    document.getElementById('artist-name').textContent = artist.name;
+
+    // Mettre à jour la liste des albums de l'artiste
+    ajaxRequest('GET', '../api.php/album/artist/' + artistId, function(albumList) {
+      console.log(albumList);
+      const albumListContainer = document.getElementById('album-list-container');
+      for (let i = 0; i < albumList.length; i++) {
+        const album = albumList[i];
+        const albumHtml = '<div class="album">' +
+          `<img src="${album.imageUrl}" alt="album">` +
+          '<p class="album_title">' + album.title + '</p>' +
+          '<p class="album_year">' + album.releaseDate + '</p>' +
+          '</div>';
+        albumListContainer.insertAdjacentHTML('beforeend', albumHtml);
+      }
+    });
+  });
+}
