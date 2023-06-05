2480f6240f62f0b8cfea0af3a05ed144fbfc05d6
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-06-05 14:12:01 +0200

    bugfix

--- sql/model.sql
+++ sql/model.sql
@@ -104,7 +104,7 @@ CREATE TABLE public.playlist_(
 CREATE TABLE public.playlist_track_(
                                        id_track      INT  NOT NULL ,
                                        id_playlist   INT  NOT NULL ,
-                                       add_date      DATE  NOT NULL DEFAULT now(),
+                                       add_date      TIMESTAMP  NOT NULL DEFAULT now(),
                                        CONSTRAINT playlist_track__PK PRIMARY KEY (id_track,id_playlist)
 );
 
@@ -113,10 +113,11 @@ CREATE TABLE public.playlist_track_(
 -- Table: history_
 ------------------------------------------------------------
 CREATE TABLE public.history_(
+                                id_history SERIAL NOT NULL ,
                                 id_track   INT  NOT NULL ,
                                 id_user    INT  NOT NULL ,
-                                add_date   DATE  NOT NULL DEFAULT now(),
-                                CONSTRAINT history__PK PRIMARY KEY (id_track,id_user)
+                                add_date   TIMESTAMP  NOT NULL DEFAULT now(),
+                                CONSTRAINT history__PK PRIMARY KEY (id_history)
 );
 
 
