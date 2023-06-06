fdeb6d93a0380dcecca9cf4cfac3e0c35e86e14f
Antoine Bertin <antoine.bertin@isen-ouest.yncrea.fr>
2023-06-06 14:47:38 +0200

    album details

--- src/js/album_details.js
+++ src/js/album_details.js
@@ -15,19 +15,62 @@ if(albumId){
     document.getElementById('album-cover').src = album.imageUrl;
 
     // Mettre à jour la liste des titres
-    ajaxRequest('GET', '../api.php/track/album/' + albumId, function(trackList) {
-      console.log(trackList);
+    ajaxRequest('GET', '../api.php/playlist', function(playlists) {
+      console.log(playlists);
+      ajaxRequest('GET', '../api.php/track/album/' + albumId, (trackList) => displayTracks(trackList, playlists)); 
+    });
+
+    function displayTracks(trackList, playlists) {
+      console.log(trackList, playlists);
       const trackListContainer = document.getElementById('tracklist-container');
       for (let i = 0; i < trackList.length; i++) {
         const track = trackList[i];
-        const trackHtml = '<div class="track">' +
-            `<img src="${album.imageUrl}" alt="album">` +
-            // `<p class="track_name" onclick="playTrack('${track.audioUrl}','${track.title}')">` + (i+1) + '. ' + track.title + '</p>' +
-            `<a href="audio_player.html?id=${track.id}" class="track_name_link">${(i+1)}. ${track.title}</a>` +
-            '</div>';
-        trackListContainer.insertAdjacentHTML('beforeend', trackHtml);
+        
+        const trackHtml = document.createElement('div');
+        trackHtml.className = 'track';
+        
+        const trackImg = document.createElement('img');
+        trackImg.src = album.imageUrl;
+        trackImg.alt = 'album';
+        trackHtml.appendChild(trackImg);
+        
+        const trackNameLink = document.createElement('a');
+        trackNameLink.href = `audio_player.php?id=${track.id}`;
+        trackNameLink.className = 'track_name_link';
+        trackNameLink.textContent = `${(i+1)}. ${track.title}`;
+        trackHtml.appendChild(trackNameLink);
+        
+        const selectContainer = document.createElement('div');
+        selectContainer.className = 'select-container';
+        const select = document.createElement('select');
+        select.id = 'filter';
+        selectContainer.appendChild(select);
+        
+        const option = document.createElement('option');
+        option.name = 'searchBy';
+        option.value = 'disabled';
+        option.disabled = true;
+        option.selected = true;
+        option.textContent = 'Chose Playlist';
+
+        select.appendChild(option);
+        for (const playlist of playlists) {
+          const option = document.createElement('option');
+          option.name = 'searchBy';
+          option.value = playlist.id;
+          option.textContent = playlist.name;
+          select.appendChild(option);
+        }
+        trackHtml.appendChild(selectContainer);
+        trackListContainer.appendChild(trackHtml);
       }
-    });
+    }
+  });
+}
+
+function addTrackToPlaylist(trackId, playlistId) {
+  ajaxRequest('POST', '../api.php/playlist/' + playlistId + '/track/' + trackId, function() {
+    console.log('Track added to playlist');
   });
 }
 
@@ -37,3 +80,8 @@ function playTrack(audioUrl, title) {
   urlParams.append('title', title);
   window.location.href = 'audio_player.php?' + urlParams.toString();
 }
+
+
+
+
+
