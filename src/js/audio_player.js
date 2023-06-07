28a8b3a6e3462f1c4dae8315b435f00aad33e573
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-06-07 09:48:46 +0200

    bugfix

--- src/js/audio_player.js
+++ src/js/audio_player.js
@@ -4,27 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
   const trackTitle = document.getElementById('track-title');
   const artistName = document.getElementById('artist-name');
   const albumName = document.getElementById('album-name');
-  const progressBar = document.querySelector('.progress');
-  const playPauseButton = document.getElementById('play-pause-button');
+  // const progressBar = document.querySelector('.progress');
+  // const playPauseButton = document.getElementById('play-pause-button');
 
   // Mettre à jour la barre de progression
-  audio.addEventListener('timeupdate', () => {
-    const progress = (audio.currentTime / audio.duration) * 100;
-    progressBar.style.width = `${progress}%`;
-  });
-
-  // Mettre à jour le bouton de lecture/pause
-  playPauseButton.addEventListener('click', () => {
-    if (audio.paused) {
-      audio.play();
-      playPauseButton.classList.remove('play');
-      playPauseButton.classList.add('pause');
-    } else {
-      audio.pause();
-      playPauseButton.classList.remove('pause');
-      playPauseButton.classList.add('play');
-    }
-  });
+  // audio.addEventListener('timeupdate', () => {
+  //   const progress = (audio.currentTime / audio.duration) * 100;
+  //   progressBar.style.width = `${progress}%`;
+  // });
+  //
+  // // Mettre à jour le bouton de lecture/pause
+  // playPauseButton.addEventListener('click', () => {
+  //   if (audio.paused) {
+  //     audio.play();
+  //     playPauseButton.classList.remove('play');
+  //     playPauseButton.classList.add('pause');
+  //   } else {
+  //     audio.pause();
+  //     playPauseButton.classList.remove('pause');
+  //     playPauseButton.classList.add('play');
+  //   }
+  // });
 
   // Récupérer l'id de la track à partir des paramètres de l'URL
   const params = new URLSearchParams(document.location.search);
