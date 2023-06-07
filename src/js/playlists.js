439da8ec9b087236a9e0e97b399dab8e1e5443c9
Florent Butery <florent.butery@isen-ouest.yncrea.fr>
2023-06-07 09:18:50 +0200

    button delete playlist

--- src/js/playlists.js
+++ src/js/playlists.js
@@ -4,11 +4,33 @@ function displayPlaylists(playlistList) {
     playlistListContainer.innerHTML = '';
     for (let i = 0; i < playlistList.length; i++) {
         const playlist = playlistList[i];
-        const playlistHtml = '<div class="playlist">' +
-            `<a href="playlist_details.php?id=${playlist.id}" class="playlist_name_link">${playlist.name} - ${playlist.creationDate}</a>` +
-            '</div>';
-        playlistListContainer.insertAdjacentHTML('beforeend', playlistHtml);
+
+        const playlistHtml = document.createElement('div');
+        playlistHtml.className = 'playlist';
+        playlistHtml.innerHTML = `<a href="playlist_details.php?id=${playlist.id}" class="playlist_name_link">${playlist.name} - ${playlist.creationDate}</a>`;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-track';
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function() {
+            deletePlaylist(playlist.id);
+        });
+        playlistHtml.appendChild(deleteButton);
+
+        playlistListContainer.appendChild(playlistHtml);
     }
+}
+
+function deletePlaylist(playlistId) {
+    ajaxRequest(
+        'DELETE',
+        '../api.php/playlist',
+        function () {
+            console.log('Playlist deleted');
+            window.location.reload();
+        },
+        `id=${playlistId}`
+    );
 }
 
 ajaxRequest('GET', '../api.php/playlist', displayPlaylists);
